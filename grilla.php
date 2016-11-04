7835831648bb9dda5449890020e19d7c9c4476f5
juanpabloletteri <pabloletteri@gmail.com>
2016-11-04 15:31:17 -0300

    muestra grilla ok

--- grilla.php
+++ grilla.php
@@ -6,6 +6,7 @@
 	echo "<table class='table table-hover table-responsive'>
 			<thead>
 				<tr>
+					<th>  ID   </th>
 					<th>  Patente   </th>				
 					<th>  Ingreso     </th>
 					<th>  Egreso   </th>
@@ -13,17 +14,20 @@
 				</tr> 
 			</thead>";   	
 
-		foreach ($ArrayDePersonas as $personaAux){
-
+		foreach ($ArrayDePersonas as $personaAux)
+		{
 			echo " 	<tr>
-						
-						
+						<td>".$personaAux->id."</td>
 						<td>".$personaAux->patente."</td>
 						<td>".$personaAux->ingreso."</td>
 						<td>".$personaAux->egreso."</td>
-						<td><button class='btn btn-danger' name='Borrar' onclick='Borrar(".$personaAux->GetId().")'>   <span class='glyphicon glyphicon-remove-circle'>&nbsp;</span>Borrar</button></td>
-						<td><button class='btn btn-warning' name='Modificar' onclick='Modificar(".$personaAux->GetId().")'><span class='glyphicon glyphicon-edit'>&nbsp;</span>Modificar</button></td>
+						<td>".$personaAux->acobrar."</td>
+						<td><button name='Borrar' onclick='Borrar(".$personaAux->id.")'>   <span class='glyphicon glyphicon-remove-circle'>&nbsp;</span>Borrar</button></td>
+						<td><button name='Modificar' onclick='Modificar(".$personaAux->id.")'><span class='glyphicon glyphicon-edit'>&nbsp;</span>Modificar</button></td>
 					</tr>";
 		}	
-	echo "</table>";		
+	echo "</table>";	
+//var_dump($ArrayDePersonas);
+
+
 ?>

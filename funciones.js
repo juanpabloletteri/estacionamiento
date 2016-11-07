095af55d53fc8552e38a549f5c313bc1f367bb47
juanpabloletteri <pabloletteri@gmail.com>
2016-11-07 01:29:54 -0300

    facturar

--- funciones.js
+++ funciones.js
@@ -9,8 +9,22 @@ function ingresar()
 			patente: $("#patente").val(),
 		}});
 	success: alert("GESTIONANDO AUTO");
+
+	f.done (function(resultado)
+		{
+			if (resultado=="ingresar")
+			{
+				alert("Auto Ingresado Exitosamente");
+			}
+			else
+			{
+				alert("A facturar: $" + resultado);
+			}
+			
+		});
+
 	//por si quiero imprimir la tabla grilla
-	f.done (function()
+	/*f.done (function()
 			{
 				var d=$.ajax({
 				url:"grilla.php",
@@ -23,7 +37,7 @@ function ingresar()
 					}
 				);
 			}
-		);
+		);*/
 
 }
 
--- nexo.php
+++ nexo.php
@@ -13,14 +13,17 @@ if (isset($_POST['boton']))
 
 		if ($auto->id==NULL)
 		{
-			echo ("INGRESAR");
+			echo ("ingresar");
 			vehiculo::ingresar($_POST['patente']);
 		}
 		else
 		{
-			echo("COBRAR");
+			//echo("cobrar");
 			$horas=(strtotime('now') - strtotime($auto->ingreso))/60/60 ;
-			var_dump($horas);
+			//var_dump($horas);ç
+			$monto=CalcularMonto($horas);
+			//var_dump($monto);
+			echo($monto);
 		}
 
 		//var_dump(vehiculo::TraerUnVehiculo($_POST['patente']));	
@@ -51,6 +54,47 @@ if (isset($_POST['boton']))
 		session_unset();
 		session_destroy();
 		echo "salir";
+	}
+}
+
+function CalcularMonto($horas)
+{
+	$monto=10;
+	if ($horas > 0 && $horas <= 1)
+	{
+		return $monto;
+	}
+	elseif ($horas > 1 && $horas <= 2)
+	{
+		return $monto*2;
+	}
+	elseif ($horas > 2 && $horas <= 3)
+	{
+		return $monto*3;
+	}
+	elseif ($horas > 3 && $horas <= 4)
+	{
+		return $monto*4;
+	}
+	elseif ($horas > 4 && $horas <= 5)
+	{
+		return $monto*5;
+	}
+	elseif ($horas > 5 && $horas <= 6)
+	{
+		return $monto*6;
+	}
+	elseif ($horas > 6 && $horas <= 7)
+	{
+		return $monto*7;
+	}
+	elseif ($horas > 7 && $horas <= 8)
+	{
+		return $monto*8;
+	}
+	elseif ($horas > 8)
+	{
+		return $monto*10;
 	}
 }
 
